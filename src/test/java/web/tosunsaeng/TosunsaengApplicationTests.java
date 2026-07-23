7b30dff71ce9a584348d350f0d1e191c5615f590
msde76 <msde7648@naver.com>
2026-07-23 14:47:11 +0900

    chore: initialize learning core environment

--- src/test/java/web/tosunsaeng/TosunsaengApplicationTests.java
+++ src/test/java/web/tosunsaeng/TosunsaengApplicationTests.java
@@ -2,7 +2,9 @@ package web.tosunsaeng;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class TosunsaengApplicationTests {
 
